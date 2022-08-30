abb3189a033bc1c7fdf1f27afeb075371129da04
Replit user <>
2022-08-30 22:38:28 +0200

    completed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer= require('multer');
+const upload = multer({dest:'uploads/'})
 
 var app = express();
 
@@ -12,9 +14,19 @@ app.get('/', function (req, res) {
 });
 
 
-
+//use multer as middleware
+app.post('/api/fileanalyse', upload.single('upfile'), (req,res) => {
+  console.log(req.file)
+  let name = req.file.originalname;
+  let type = req.file.mimetype;
+  let size = req.file.size;
+  res.json({name:name, type:type, size:size});
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+//TODO THINGS
+//This project look easy, the input with type field in the html lets you upload the file and no database are involved, then the only requisite is to manage the post petition, read the metadata and redirect to a JSON response ive seeing that multer can help with that
